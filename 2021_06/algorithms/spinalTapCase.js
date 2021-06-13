5249cdc12ddf51d33471a3b2b93e672b8c04f001
Joel Fernandez <jfernandezyt@outlook.com>
2021-06-12 20:28:14 -0400

    finally finished the spinal tap case algo

--- 2021_06/algorithms/spinalTapCase.js
+++ 2021_06/algorithms/spinalTapCase.js
@@ -1,37 +1,50 @@
 /*
-
 Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 */
 
-hasCapital = new RegExp(/[A-Z]+/, 'g');
-wantedResult = new RegExp(/[a-z,-]/, 'g');
-
 function spinalCase(str) {
-    let hasSpaces, wantedResult, hasCapital, test, total = 0, temp;
+    let hasSpaces, wantedResult, hasCapital, arr = [], extraCapitalLet = [], tempString = '', goodString;
 
     hasSpaces = new RegExp(/[' ', _ , -]+/, 'g');
     wantedResult = new RegExp(/[a-z,-]/, 'g');
     hasCapital = new RegExp(/[A-Z]+/, 'g');
 
     str = str.replace(hasSpaces, '-');
-    test = str.toLowerCase();
 
-    while (wantedResult.exec(test) !== null) {
-        total++;
-    }
-
-    while (hasCapital.exec(str) !== null) {
-        console.log(str[hasCapital.lastIndex - 2]);
-        // if(str[hasCapital.lastIndex - 2] != '-'){
-        //     temp = str.slice(0, )
-        // }
-    }
-
-    if(total === str.length){
-        return str.toLowerCase();
-    }
+    arr = str.split(hasSpaces);
+    
+    arr.forEach(element => {
+        let total = 0;
+        let temp = [];
+        while (hasCapital.exec(element) !== null) {
+            temp.push(hasCapital.lastIndex - 1);
+            total++
+        }
+        if(total > 1){
+            if(temp[0] === 0){
+                total -= 1;
+            }
+            extraCapitalLet = temp.slice(-1 * total);
+        }else{
+            goodString = arr.join('-');
+        }
+    });
+    if(extraCapitalLet.length  === 0){
+        return goodString.toLowerCase();
+    }else{
+        tempString += str.slice(0, extraCapitalLet[0]);
+        for (let i = 0; i < extraCapitalLet.length; i++) {
+            const index = extraCapitalLet[i];
+            if(extraCapitalLet.length > 1 && i !== extraCapitalLet.length - 1 ){
+                tempString += '-' + str.slice(index, extraCapitalLet[i + 1]);
+            }else{
+                tempString += '-' + str.slice(index);
+            }            
+        }
+        return tempString.toLowerCase();
+    }  
 }
-console.log(spinalCase('AllThe-small Things'));
+console.log(spinalCase('This Is Spinal Tap'), 1)
 /*
 thisIsSpinalTap
 AllThe-small Things
